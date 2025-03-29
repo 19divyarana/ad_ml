ebcd7617da71256eb1c03517c45f3c0756287f87
19divyarana <ranadivya301@gmail.com>
2025-03-29 10:34:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load the trained model
-model = joblib.load('fraud_detection_model.pkl')
+model = joblib.load('fraud_model.pkl')
 
 @app.route('/')
 def home():
